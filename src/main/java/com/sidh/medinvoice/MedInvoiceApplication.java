55a5ac36142890100a38915210a078d9292c9424
sidhartha07 <sidhartha.jobies@gmail.com>
2024-02-28 09:28:44 +0530

    initial commit

---
+++ src/main/java/com/sidh/medinvoice/MedInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.sidh.medinvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedInvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidh/medinvoice/MedInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidh.medinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
