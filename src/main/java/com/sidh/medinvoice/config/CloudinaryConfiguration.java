2f460d205bcf8132a75a3f871615fc0b102ff2de
sidhartha07 <sidhartha.jobies@gmail.com>
2024-03-07 12:40:16 +0530

    added Dockerfile, moved to cloud DB instance

--- src/main/java/com/sidh/medinvoice/config/CloudinaryConfiguration.java
+++ src/main/java/com/sidh/medinvoice/config/CloudinaryConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CloudinarConfiguration {
+public class CloudinaryConfiguration {
     @Value("${cloudinary.cloud_name}")
     private String cloudName;
 
